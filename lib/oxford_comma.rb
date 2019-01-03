f0a27c61ee5d35767adfc185238a8eca7a5227c7
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-03 18:43:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  short = array.pop
-  #short_str = short.join(", ")
+  array.pop
+  short_str = array.join(", ")
   #ending = " and #{array[-1]}"
   #short_str + ending
 end
